855d63e66db49dbea4546e57d610822b43dfc98e
TheSilkMiner <thesilkminer@outlook.com>
2020-04-28 15:24:36 +0200

    Fix missing sided annotation
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/kotlin/net/thesilkminer/mc/prjtags/client/RegistrationHandler.kt
+++ src/main/kotlin/net/thesilkminer/mc/prjtags/client/RegistrationHandler.kt
@@ -2,10 +2,11 @@ package net.thesilkminer.mc.prjtags.client
 
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.relauncher.Side
 import net.thesilkminer.mc.boson.api.event.ConfigurationRegisterEvent
 import net.thesilkminer.mc.prjtags.MOD_ID
 
-@Mod.EventBusSubscriber(modid = MOD_ID)
+@Mod.EventBusSubscriber(modid = MOD_ID, value = [Side.CLIENT])
 @Suppress("unused")
 object RegistrationHandler {
     @SubscribeEvent
